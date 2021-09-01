b24ac43525e3c9d474857e906d1ee3ab85d92efc
DolphaGo <adamdoha@naver.com>
2021-09-02 00:48:07 +0900

    style scoped를 쓰는 이유

---
+++ ch09/styletest/src/components/Child1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main test">
+    {{ msg }}
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'child1',
+  data() {
+    return {
+      msg: 'Child1'
+    }
+  }
+}
+</script>
+<style scoped>
+.main {
+  border: solid 1px black;
+  background-color: yellow;
+}
+</style>
